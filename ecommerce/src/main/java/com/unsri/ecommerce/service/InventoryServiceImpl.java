424eaeabc227d97c2802556626380ba7ebce8825
faisal.morensya <faisal.morensya@talabat.com>
2021-04-24 18:33:57 +0400

    init base repo

---
+++ ecommerce/src/main/java/com/unsri/ecommerce/service/InventoryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.unsri.ecommerce.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.unsri.ecommerce.models.Inventory;
+import com.unsri.ecommerce.repository.InventoryRepository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryServiceImpl implements InventoryService {
+
+    private InventoryRepository _repository;
+
+    public InventoryServiceImpl(InventoryRepository repository) {
+        _repository = repository;
+    }
+
+    @Override
+    public List<Inventory> GetAllItems() {
+        
+        List<Inventory> items = new ArrayList<Inventory>();
+        _repository.findAll().forEach(item -> items.add(item));
+
+        return items;
+    }
+    
+}
---
+++ ecommerce/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO INVENTORY (item_name, price) VALUES ('Microsoft Surface', 10.00);
+INSERT INTO INVENTORY (item_name, price) VALUES ('Macbook Pro', 10.20);
+INSERT INTO INVENTORY (item_name, price) VALUES ('Macbook Air', 10.30);
