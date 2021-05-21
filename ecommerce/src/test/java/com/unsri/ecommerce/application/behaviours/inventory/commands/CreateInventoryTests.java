b6579f5855bbbe01f7683e09fe1f442a8f16d609
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-05-21 18:13:55 +0700

    update unittest

--- ecommerce/src/test/java/com/unsri/ecommerce/application/behaviours/inventory/commands/CreateInventoryTests.java
+++ ecommerce/src/test/java/com/unsri/ecommerce/application/behaviours/inventory/commands/CreateInventoryTests.java
@@ -4,31 +4,33 @@ import com.unsri.ecommerce.domain.models.Inventory;
 import com.unsri.ecommerce.infrastructure.repository.InventoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.util.Assert;
 
 import static org.mockito.Mockito.*;
 
 public class CreateInventoryTests {
 
+    @Mock
     private InventoryRepository inventoryRepository;
 
+    @InjectMocks
     private CreateInventory createInventory;
+
     private Inventory inventory;
 
     @BeforeEach
     public void setup() {
-        inventory = new Inventory();
-        inventory.setItemName("Apple");
-        inventory.setPrice(1000.0);
-
-        mock(InventoryRepository.class);
-
-        createInventory = new CreateInventory(inventory, inventoryRepository);
     }
 
     @Test
     public void CreateInventoryTests_ReturnSuccess() {
         // Arrange
+        inventory = new Inventory();
+        inventory.setItemName("Apple");
+        inventory.setPrice(1000.0);
+
         when(inventoryRepository.save(inventory)).thenReturn(inventory);
 
         // Act
@@ -36,8 +38,8 @@ public class CreateInventoryTests {
 
         // Assert
         Assert.isTrue(expectedResult != null, "should not null");
-        Assert.isTrue(expectedResult.getItemName() == inventory.getItemName(), "item name should be same");
-        Assert.isTrue(expectedResult.getPrice() == inventory.getPrice(), "price should be same");
+        Assert.isTrue(expectedResult.getItemName().equals(inventory.getItemName()), "item name should be same");
+        Assert.isTrue(expectedResult.getPrice().equals(inventory.getPrice()), "price should be same");
  
         // Verify
         verify(inventoryRepository, times(1)).save(inventory);
