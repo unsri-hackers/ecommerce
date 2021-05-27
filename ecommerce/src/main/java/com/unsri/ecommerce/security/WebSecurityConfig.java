f11c23a9773fccb5bfa5898de84196e07d16d73a
faisal.morensya <faisal.morensya@talabat.com>
2021-05-27 09:54:05 +0400

    add lib

--- ecommerce/src/main/java/com/unsri/ecommerce/security/WebSecurityConfig.java
+++ ecommerce/src/main/java/com/unsri/ecommerce/security/WebSecurityConfig.java
@@ -1,4 +1,7 @@
 package com.unsri.ecommerce.security;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class WebSecurityConfig {
 }
